295e58625ecc5a2b2c5d16cc0c8203403263b0ef
neuroflip <neuroflip@gmail.com>
2023-11-09 19:21:56 +0100

    Solved bug in fileLoader

--- src/components/GpxRouteMap/FileLoader.tsx
+++ src/components/GpxRouteMap/FileLoader.tsx
@@ -6,31 +6,27 @@ interface FileLoaderProps {
 }
 
 function  FileLoader({ onFileLoaded }: FileLoaderProps) {
-    const fileSelect = document.getElementById('fileSelect')
-    const fileElem = document.getElementById('fileElem')
-
-    fileSelect?.addEventListener('click', () => {
-        fileElem?.click();
-    }, false);
-
-    fileElem?.addEventListener('change', (e: Event) => {
-        const file = (e?.target as HTMLInputElement).files?.[0]
-        const fr = new FileReader()
-
-        fr.onload = () => {
-          onFileLoaded(fr.result?.toString() || '')
-        }
-        file && fr.readAsText(file)
-    })
+    React.useEffect(() => {
+        const fileSelect = document.getElementById('fileSelect')
+        const fileElem = document.getElementById('fileElem')
+    
+        fileSelect?.addEventListener('click', () => {
+            fileElem?.click();
+        }, false);
+    
+        fileElem?.addEventListener('change', (e: Event) => {
+            const file = (e?.target as HTMLInputElement).files?.[0]
+            const fr = new FileReader()
+    
+            fr.onload = () => {
+              onFileLoaded(fr.result?.toString() || '')
+            }
+            file && fr.readAsText(file)
+        })    
+    }, [])
 
     return <>
-        <input type="file"
-        id="fileElem"
-        multiple
-        accept=".gpx"
-        style={{
-            display: 'none'
-        }} />
+        <input type="file" id="fileElem" multiple accept=".gpx" style={{ display: 'none' }} />
         <button id="fileSelect" type="button">Select some files</button>
     </>
 }
